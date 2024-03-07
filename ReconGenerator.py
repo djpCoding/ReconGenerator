05bb85eca0f02d0b5a53f384074e625a92f019f3
djpCoding <119716129+djpCoding@users.noreply.github.com>
2024-03-06 18:07:02 -0600

    Additional Budget Prediction added

--- ReconGenerator.py
+++ ReconGenerator.py
@@ -21,16 +21,31 @@ def get_recon_base_model():
     recon_base_model = pickle.load(open('xgb_recon_regress.pkl', "rb"))
     return recon_base_model
 
-#@st.cache_data
-#def get_budget_data():
-#    full_data = pd.read_csv('CT_LEDGER.csv')
-#    return full_data
-
 recon_clf = get_recon_base_model()
-#full_data = get_budget_data()
+
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
+@st.cache_data
+def get_dep_data():
+    actuals = pd.read_csv('departmentspend.csv')
+    budget_prediction = pd.read_csv('budgetprediction.csv')
+    budget_prediction[['YEAR','MONTH']] = budget_prediction['Date'].str.split('-',expand=True)
+    annual_budg_pred = budget_prediction.groupby(['YEAR','Department'], as_index=False)[['Amount']].sum()
+    annual_budg_pred['YEAR'] = annual_budg_pred['YEAR'].astype(int)
+    annual_budg_pred.loc[annual_budg_pred['YEAR'] <= 2023, 'Amount'] = 0
+    return actuals, annual_budg_pred
+
+actuals, annual_budg_pred = get_dep_data()
+
+@st.cache_resource
+def get_department_options(spend_df):
+    departments = spend_df['Department'].unique()
+    department_list = departments.tolist()
+    department_list = sorted(department_list) 
+    return department_list  
+
+department_list = get_department_options(annual_budg_pred)
 
 
 #st.title()
@@ -173,6 +188,40 @@ def display_model_info():
     st.markdown('This table offers a short description of the variables used in building this ML model.')
     st.markdown(variable_desc_df.style.hide(axis="index").to_html(), unsafe_allow_html=True) 
 
+
+def plot_budget_projection(department):
+    in_var_df = actuals.loc[actuals['Department'] == department]
+    pred_df = annual_budg_pred.loc[annual_budg_pred['Department'] == department]
+
+
+    pred_df['Amount'].loc[pred_df['YEAR'] == 2024] = round(annual_budg_pred['Amount'].loc[(annual_budg_pred['YEAR'] == 2024) & (annual_budg_pred['Department'] == department)].tolist()[0] - actuals['Amount'].loc[(actuals['Fiscal Year'] == 2024) & (actuals['Department'] == department)].tolist()[0])
+
+
+    new_year = {'Fiscal Year': 2025, 'Department': department, 'Amount': 0}
+    new_year_df = pd.DataFrame(new_year, index=[0])
+    in_var_df2 = pd.concat([in_var_df, new_year_df], ignore_index=True)
+
+
+
+    plt.bar(in_var_df2['Fiscal Year'], in_var_df2['Amount'], color='slategrey')
+    plt.bar(pred_df['YEAR'], pred_df['Amount'], bottom=in_var_df2['Amount'], color = 'dodgerblue')    
+
+    plt.legend(['Actuals','Forecasted'])
+    plt.title(department)
+    plt.yticks([])
+    plt.show()
+    fig = plt.show()
+    st.pyplot(fig)
+
+    pred_df_2 = pred_df.loc[pred_df['YEAR'] > 2023]
+    pred_df_2 = pred_df_2.rename(columns={'YEAR':'Fiscal Year'})
+
+    st.markdown('The table below outlines the values associated with the chart above. Pleasr review for detailed information')
+    st.subheader('Actuals:')
+    st.markdown(in_var_df2.style.format({"Amount": "{:.2f}"}).hide(axis="index").to_html(), unsafe_allow_html=True)
+    st.subheader('Projected Additional Spend:')
+    st.markdown(pred_df_2.style.format({"Amount": "{:.2f}"}).hide(axis="index").to_html(), unsafe_allow_html=True)
+
 if add_radio == "Reconciliation Manager":
     
     st.title("Reconciliation Manager")
@@ -186,4 +235,14 @@ if add_radio == "Reconciliation Manager":
 
 elif add_radio == "Budgeting and Forecasting":
     st.header('Budgeting and Forecasting')
-    st.markdown('This portion of the site is still under construction. The existing ARIMA model is not optimized or suitable to publish against free cloud memory stores.')
+    initial_selection = st.selectbox('Select Option', ['Select an option to begin','Review Previous Actuals', 'Submit Budgets for Review'])
+    if initial_selection == 'Review Previous Actuals':
+        st.subheader('Department Selection')
+        department_selected = st.selectbox('Please select a department that you would like to review actuals and 2025 forecasts for.', department_list)
+        st.markdown("This model uses a Seasonal Autoregressive Integrated Moving Average to more accurately determine future spend for 2024 and 2025. While 2024 is considered highly accurate, these models in their current state of this demo may show some variance in 2025.")
+        go = st.button('Go')
+
+        if go:
+            plot_budget_projection(department_selected)
+    elif initial_selection == 'Submit Budgets for Review':
+        st.write('Under Construction')
